d0c3162b6e30efa15208eac75d1264dad3f30882
anshit-1223 <anshit1223@gmail.com>
2026-08-20 23:16:17 +0530

    created variable.tf

--- ec2.tf
+++ ec2.tf
@@ -46,10 +46,10 @@ resource "aws_security_group" "sg_ec2" {
 resource "aws_instance" "my_instance" {
   key_name        = aws_key_pair.ec2_key.key_name
   security_groups = [aws_security_group.sg_ec2.name]
-  instance_type   = "t2.micro"
-  ami             = "ami-01a00762f46d584a1"
+  instance_type   = var.instance_type
+  ami             = var.instance_ami
   root_block_device {
-    volume_size = 15
+    volume_size = var.instance_size
     volume_type = "gp3"
   }
 
